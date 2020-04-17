c92066b61abed9add9d846361f069a904dddbc75
swiarda <63777776+swiarda@users.noreply.github.com>
2020-04-17 18:03:45 +0200

    Updated DataFunction
    Paar kleine aanpassingen aan de DataFunction

--- DataFunction.py
+++ DataFunction.py
@@ -8,7 +8,8 @@ def generateData(path):
     import numpy as np
     import pandas as pd
     import statistics as st
-    
+    import math 
+
     Data = pd.ExcelFile(path)
     
     ProductSize = pd.read_excel(Data,"ProductSize")
@@ -22,7 +23,7 @@ def generateData(path):
     ## ProductInches including the uncertainty for TV screensize
     probability = [0.25,0.5,0.25] # same for all uncertain bandwiths
     outcome = [-2,0,1]
-    M = np.zeros((len(ProductSize),1))
+    M = np.zeros((len(ProductSize),5))
     ProductInches = pd.DataFrame(M,index = ProductSize.Format)
     
     for i in range(6):
@@ -30,6 +31,23 @@ def generateData(path):
     for j in range(6,12):
         ProductInches.values[j,0] = ProductSize.values[j,0] + np.random.choice(outcome,1,probability)
     
+    for i in range(9,12):
+        ProductInches.values[i,1] = math.atan(ProductFormat.values[0,1]/ProductFormat.values[1,1])
+    for i in range(6,9):
+        ProductInches.values[i,1] = math.atan(ProductFormat.values[0,2]/ProductFormat.values[1,2])
+    ProductInches.values[5,1] = math.atan(ProductFormat.values[0,3]/ProductFormat.values[1,3])
+    ProductInches.values[4,1] = math.atan(ProductFormat.values[0,6]/ProductFormat.values[1,6])
+    ProductInches.values[3,1] = math.atan(ProductFormat.values[0,9]/ProductFormat.values[1,9])
+    ProductInches.values[2,1] = math.atan(ProductFormat.values[0,5]/ProductFormat.values[1,5])
+    ProductInches.values[1,1] = math.atan(ProductFormat.values[0,8]/ProductFormat.values[1,8])
+    ProductInches.values[0,1] = math.atan(ProductFormat.values[0,7]/ProductFormat.values[1,7])
+    
+    for i in range(12):
+        ProductInches.values[i,2]= (math.cos(ProductInches.values[i,1])*ProductInches.values[i,0]*0.0254)+(ProductSize.values[i,3]/1000)
+        ProductInches.values[i,3]= (math.sin(ProductInches.values[i,1])*ProductInches.values[i,0]*0.0254)+(ProductSize.values[i,4]/1000)
+        ProductInches.values[i,4] = ProductSize.values[i,5]/1000
+        
+    ProductInches = ProductInches.rename(columns = {4:'Exclusion (m)',0:'Diagonal (inches)',1:'Angle',2:'Height (m)',3:'Width (m)'})
     ## Prices per product over time including the uncertainty
     outcome2 = [0.8,1,1.2] ## Plus or minus 20%
     Price2 = pd.DataFrame(np.zeros((12,15)), index = Price.Format, columns = Price.columns[4:19])
@@ -41,7 +59,7 @@ def generateData(path):
     
     outcome3 = [0.9,1,1.1] ##Plus or minus 10%
     CostSubstrate2 = pd.DataFrame(np.zeros((1,len(CostSubstrate.columns[3:18]))), columns = CostSubstrate.columns[3:18])
-    for i in range(len(CostSubstrate.columns[3:18])):
+    for i in range(2,len(CostSubstrate.columns[3:18])):
         CostSubstrate2.values[0,i] = np.random.choice(outcome3,1,probability) * CostSubstrate.values[0,i+3]
     
     ## Investment Cost over time including the uncertainty
@@ -52,11 +70,16 @@ def generateData(path):
     
     ## Yield per market over time including the uncertainty
     outcome4 = [0.85,1,1.02] ## 15% down or 2% up   
-    Yield2 = pd.DataFrame(np.zeros((3,len(Yield.columns[3:18]))), index = Yield.Yieldpermarket, columns = Yield.columns[3:18])
-    for i in range(len(Yield.Yieldpermarket)):
-        for j in range(len(Yield.columns[3:18])):
-            Yield2.values[i,j] = np.random.choice(outcome4,1,probability) * Yield.values[i,j+3]
-    
+    Yield2 = pd.DataFrame(np.zeros((len(ProductInches),len(Yield.columns[3:18]))), index = ProductInches.index, columns = Yield.columns[3:18])
+
+    for j in range(2,len(Yield.columns[3:18])):
+        Yield2.values[0:2,j] = np.random.choice(outcome4,1,probability) * Yield.values[0,j+3]
+
+    for j in range(2,len(Yield.columns[3:18])):
+        Yield2.values[2:5,j] = np.random.choice(outcome4,1,probability) * Yield.values[1,j+3]
+
+    for j in range(2,len(Yield.columns[3:18])):
+        Yield2.values[5:len(ProductInches),j] = np.random.choice(outcome4,1,probability) * Yield.values[2,j+3]
            
     MaxCapacity = Parameters.values[0,3]
     RD = np.random.choice([0.04,0.05,0.11],1,probability) ## R&D
@@ -69,7 +92,7 @@ def generateData(path):
     DPO = np.random.choice([35,45,55],1,probability)
     DSO = np.random.choice([35,45,55],1,probability)
     DIO = np.random.choice([20,30,40],1,probability)
-    return {"ProductInches": ProductInches,
+    return {"ProductSize": ProductInches,
             "ProductFormat": ProductFormat,
             "ProductPrice": Price2,
             "SubstrateCost": CostSubstrate2,
