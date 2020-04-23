834980bda3d13950aae2fc29e27d6de9f052b226
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-04-23 16:56:26 +0200

    Generalise widths

--- MAIN.py
+++ MAIN.py
@@ -12,25 +12,26 @@ import pandas as pd
 import time
 
 Scenarios = 10
+n_width = 18
+n_height = 12
+
+width = np.array([1.85-0.05*i for i in range(n_width)])
+height = np.array([1.55-0.05*i for i in range(n_height)])
 
 start_time = time.time()
 Data = {}
 for i in range(Scenarios):
     Data[i] = generateData("DataPBAS.xlsx")
- 
-end_time = time.time()    
+
+print(f"Data generation took {time.time()-start_time} seconds")
+
 Products = len(Data[0]['ProductSize'])
 Time = len(Data[0]['ProductPrice'].columns)
 
-width = np.array([1+0.05*i for i in range(18)])
-height = np.array([1+0.05*i for i in range(12)])
-
-
-
-NPV = pd.DataFrame(np.zeros((len(height),len(width))), index = height, columns = width)
+NPV = pd.DataFrame(np.zeros((n_height, n_width)), index = height, columns = width)
 for w in range(len(width)):
     for h in range(len(height)):
         NPV_ = NPV_SAA(Data,width[w],height[h])
         NPV.values[h,w] = NPV_['Average NPV']
 
-NPV.to_csv('NPV Table')
+NPV.to_csv('NPV Table.csv')
