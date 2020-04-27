0637bdbc64fda2de810226815fccd76f9ecffe54
Mike Weltevrede <46759318+mikeweltevrede@users.noreply.github.com>
2020-04-27 11:40:04 +0200

    Move Data Import into py script over notebook

--- DataFunction.py
+++ DataFunction.py
@@ -1,147 +1,116 @@
-# -*- coding: utf-8 -*-
 """
 GROUP 6 PBAS
 Data import file, incl randomness.
 """
-def generateData(path):
 
+
+def generateData(path):
+    """
+    Imports the data and constructs a random instance
+
+    Parameters
+    ----------
+    path : str
+        Path to the data
+
+    Returns
+    -------
+    dict
+        Dictionary of all needed values and randomized data
+
+    Yields
+    -------
+    KeyError
+        When the year for depreciation is not yet initialized, create it
+    """
 
     import numpy as np
     import pandas as pd
     import statistics as st
-    import math 
+    import math
 
     Data = pd.ExcelFile(path)
     ProductSize = pd.read_excel(Data, "ProductSize")
     ProductFormat = pd.read_excel(Data, "ProductFormat")
-    Price = pd.read_excel(Data, "Price")
 
     CostSubstrate = pd.read_excel(Data, "CostSubstrate")
     CostInvestment = pd.read_excel(Data, "CostInvestment")
     Yield = pd.read_excel(Data, "Yield")
-    Parameters = pd.read_excel(Data, "CostParameters")
+    Parameters = pd.read_excel(Data, "CostParameters").set_index('Cost Type')
 
-    # ProductInches including the uncertainty for TV screensize
-    probability = [0.25, 0.5, 0.25]  # same for all uncertain bandwiths
-    outcome = [-2, 0, 1]
-    M = np.zeros((len(ProductSize), 5))
-    ProductInches = pd.DataFrame(M, index=ProductSize.Format)
+    num_products = len(ProductSize)
+    ProductMeta = ProductSize[['Size (inches)', 'Format', 'Market']].copy()
 
-    for i in range(5):  # SDTV was assumed als certain, maar is ook uncertain.
-        ProductInches.values[i, 0] = ProductSize.values[i, 0]
-    for j in range(5, 12):
-        ProductInches.values[j, 0] = ProductSize.values[j, 0] + \
-            np.random.choice(outcome, 1, probability)
+    # ProductInches including the uncertainty for TV screen size
+    probability = [0.25, 0.5, 0.25]
+    bandwidths_tv = [-2, 0, 1]
 
-    for i in range(9, 12):
-        ProductInches.values[i, 1] = math.atan(
-            ProductFormat.values[0, 1]/ProductFormat.values[1, 1])
-    for i in range(6, 9):
-        ProductInches.values[i, 1] = math.atan(
-            ProductFormat.values[0, 2]/ProductFormat.values[1, 2])
-    ProductInches.values[5, 1] = math.atan(ProductFormat.values[0, 3]/ProductFormat.values[1, 3])
-    ProductInches.values[4, 1] = math.atan(ProductFormat.values[0, 6]/ProductFormat.values[1, 6])
-    ProductInches.values[3, 1] = math.atan(ProductFormat.values[0, 9]/ProductFormat.values[1, 9])
-    ProductInches.values[2, 1] = math.atan(ProductFormat.values[0, 5]/ProductFormat.values[1, 5])
-    ProductInches.values[1, 1] = math.atan(ProductFormat.values[0, 8]/ProductFormat.values[1, 8])
-    ProductInches.values[0, 1] = math.atan(ProductFormat.values[0, 7]/ProductFormat.values[1, 7])
+    ProductInches = ProductMeta.copy()
+    ProductInches['New Diagonal (inches)'] = ProductInches['Size (inches)'] + \
+        np.random.choice(bandwidths_tv, num_products, probability)
+    ProductInches['Angle'] = [math.atan(ProductFormat.loc[0, formt] / ProductFormat.loc[1, formt])
+                              for formt in ProductInches['Format']]
+    ProductInches['Height (m)'] = np.cos(ProductInches['Angle']) * ProductInches['New Diagonal (inches)'] * \
+        0.0254 + ProductSize['Border_H (in mm)']/1000 + ProductSize['Exclusion (in mm)']/1000
+    ProductInches['Width (m)'] = np.sin(ProductInches['Angle']) * ProductInches['New Diagonal (inches)'] * \
+        0.0254 + ProductSize['Border_V (in mm)']/1000 + ProductSize['Exclusion (in mm)']/1000
 
-    for i in range(12):
-        ProductInches.values[i, 2] = (math.cos(ProductInches.values[i, 1])
-                                      * ProductInches.values[i, 0]*0.0254) +\
-            2*(ProductSize.values[i, 3]/1000)
-        ProductInches.values[i, 3] = (math.sin(ProductInches.values[i, 1])
-                                      * ProductInches.values[i, 0]*0.0254) +\
-            2*(ProductSize.values[i, 4]/1000)
-        ProductInches.values[i, 4] = ProductSize.values[i, 5]/1000
-
-    ProductInches = ProductInches.rename(
-        columns={4: 'Exclusion (m)',
-                 0: 'Diagonal (inches)',
-                 1: 'Angle',
-                 2: 'Height (m)',
-                 3: 'Width (m)'})
     # Prices per product over time including the uncertainty
-    outcome2 = [0.8, 1, 1.2]  # Plus or minus 20%
-    Price2 = pd.DataFrame(np.zeros((12, 15)), index=Price.Format, columns=Price.columns[4:19])
-    for i in range(len(Price.columns[4:19])):
-        for j in range(len(Price.Format)):
-            Price2.values[j, i] = np.random.choice(outcome2, 1, probability) * Price.values[j, i+4]
+    bandwidths_prices = [0.8, 1, 1.2]
+    Price = pd.merge(ProductMeta.copy(), pd.read_excel(Data, "Price").drop(
+        columns='Unit'), on=['Size (inches)', 'Format', 'Market'])
+    Price = pd.concat([Price.iloc[:, :2], Price.iloc[:, 3:] * np.random.choice(
+        bandwidths_prices, Price.iloc[:, 3:].shape, probability)], axis=1)
 
     # Cost substrate per m^2 over time including the uncertainty
+    bandwidths_substrate_prices = [0.9, 1, 1.1]
+    CostSubstrate = CostSubstrate.iloc[:, 3:] * np.random.choice(
+        bandwidths_substrate_prices, len(CostSubstrate.columns[3:]), probability)
 
-    outcome3 = [0.9, 1, 1.1]  # Plus or minus 10%
-    CostSubstrate2 = pd.DataFrame(
-        np.zeros((1, len(CostSubstrate.columns[3:18]))), columns=CostSubstrate.columns[3:18])
-    for i in range(2, len(CostSubstrate.columns[3:18])):
-        CostSubstrate2.values[0, i] = np.random.choice(
-            outcome3, 1, probability) * CostSubstrate.values[0, i+3]
-
-    # Investment Cost over time including the uncertainty
-    # Plus or minus 10% so use same outcome as for the cost substrate
-    CostInvestment2 = pd.DataFrame(
-        np.zeros((1, len(CostInvestment.columns[3:18]))), columns=CostInvestment.columns[3:18])
-    for i in range(3):
-        CostInvestment2.values[0, i] = np.random.choice(
-            outcome3, 1, probability) * CostInvestment.values[0, i+3]*1000000
+    # Investment costs over time including the uncertainty
+    bandwidths_investment = [0.9, 1, 1.1]
+    CostInvestment = 10e6 * CostInvestment.iloc[:, 3:] * np.random.choice(
+        bandwidths_investment, len(CostInvestment.columns[3:]), probability)
+    CostInvestment = CostInvestment.dropna(axis=1)
 
     # Yield per market over time including the uncertainty
-    outcome4 = [0.85, 1, 1.02]  # 15% down or 2% up
-    Yield2 = pd.DataFrame(np.zeros((len(ProductInches), len(
-        Yield.columns[3:18]))), index=ProductInches.index, columns=Yield.columns[3:18])
+    bandwidths_yield = [0.85, 1, 1.02]  # 15% down or 2% up
+    Yield = Yield.drop(columns=["Blaco", "Blanco"]).copy()
+    Yield = pd.concat([Yield['Yieldpermarket'], Yield.iloc[:, 1:] * np.random.choice(
+        bandwidths_yield, (Yield.shape[0], Yield.shape[1]-1), probability)], axis=1)
+    Yield = pd.merge(ProductMeta, Yield, left_on='Market', right_on='Yieldpermarket', how='left')
 
-    for j in range(2, len(Yield.columns[3:18])):
-        Yield2.values[0:2, j] = np.random.choice(outcome4, 1, probability) * Yield.values[0, j+3]
+    # Depreciation over time
+    Depreciation = {}
+    depreciation_period = int(Parameters.loc['Depreciation years', 'Cost'])
 
-    for j in range(2, len(Yield.columns[3:18])):
-        Yield2.values[2:5, j] = np.random.choice(outcome4, 1, probability) * Yield.values[1, j+3]
+    for i in range(CostInvestment.shape[1]):
+        investment = CostInvestment.iloc[:, i] / depreciation_period
+        for j in range(depreciation_period):
+            year = investment.name + j
+            try:
+                Depreciation[year] = Depreciation[year] + investment[0]
+            except KeyError:
+                Depreciation[year] = investment[0]
 
-    for j in range(2, len(Yield.columns[3:18])):
-        Yield2.values[5:len(ProductInches), j] = np.random.choice(
-            outcome4, 1, probability) * Yield.values[2, j+3]
+    Depreciation = pd.DataFrame(Depreciation, index=[0])
 
-    # Depreciation
-    Time = len(Price2.columns)
-    FirstInvest = np.zeros((1, Time))
-    for t in range(10):
-        FirstInvest[0, t] = (CostInvestment2.values[0, 0]/10)
-    SecondInvest = np.zeros((1, Time))
-    for t in range(1, 11):
-        SecondInvest[0, t] = (CostInvestment2.values[0, 1]/10)
-    ThirdInvest = np.zeros((1, Time))
-    for t in range(2, 12):
-        ThirdInvest[0, t] = (CostInvestment2.values[0, 2]/10)
-
-    Depreciation = pd.DataFrame(np.zeros((1, Time)), columns=Yield.columns[3:18])
-    for t in range(Time):
-        Depreciation.values[0, t] = (FirstInvest[0, t] + SecondInvest[0, t] + ThirdInvest[0, t])
-
-    MaxCapacity = Parameters.values[0, 2]
-    RD = np.random.choice([0.04, 0.05, 0.11], 1, probability)  # R&D
-    SGA = np.random.choice([0.03, 0.04, 0.05], 1, probability)  # SG&A
-    WACC = Parameters.values[3, 2]
-    Depreciation_years = Parameters.values[4, 2]
-    maximum_width = 1.55
-    maximum_height = 1.85
-    TaxRate = np.random.choice([0.20, 0.25, 0.30], 1, probability)
-    DPO = np.random.choice([35, 45, 55], 1, probability)
-    DSO = np.random.choice([35, 45, 55], 1, probability)
-    DIO = np.random.choice([20, 30, 40], 1, probability)
+    # Construct results
     return {"ProductSize": ProductInches,
             "ProductFormat": ProductFormat,
-            "ProductPrice": Price2,
-            "SubstrateCost": CostSubstrate2,
-            "InvestmentCost": CostInvestment2,
-            "Yield": Yield2,
-            "MaxCapacity": MaxCapacity,
-            "R&D": RD.item(),
-            "SG&A": SGA.item(),
-            "WACC": WACC,
-            "Depreciation_years": Depreciation_years,
-            "Max_width": maximum_width,
-            "Max_height": maximum_height,
-            "TaxRate": TaxRate.item(),
-            "DPO": DPO.item(),
-            "DSO": DSO.item(),
-            "DIO": DIO.item(),
-            "Depreciation": Depreciation}
+            "ProductPrice": Price,
+            "SubstrateCost": CostSubstrate,
+            "InvestmentCost": CostInvestment,
+            "Yield": Yield,
+            "Depreciation": Depreciation,
+            "MaxCapacity": Parameters.loc['Max capacity', 'Cost'],
+            "R&D": np.random.choice([0.04, 0.05, 0.11], 1, probability).item(),
+            "SG&A": np.random.choice([0.03, 0.04, 0.05], 1, probability).item(),
+            "WACC": Parameters.loc['WACC', 'Cost'],
+            "Depreciation_years": depreciation_period,
+            "Max_width": 1.55,
+            "Max_height": 1.85,
+            "TaxRate": np.random.choice([0.20, 0.25, 0.30], 1, probability).item(),
+            "DPO": np.random.choice([35, 45, 55], 1, probability).item(),
+            "DSO": np.random.choice([35, 45, 55], 1, probability).item(),
+            "DIO": np.random.choice([20, 30, 40], 1, probability).item()}
