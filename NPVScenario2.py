7b433657ed7e9b7e17542ba58acb50734c32e6e3
swiarda <63777776+swiarda@users.noreply.github.com>
2020-04-29 15:42:24 +0200

    NPV functions
    3 different scenarios

--- NPVScenario2.py
+++ NPVScenario2.py
@@ -1,119 +1,125 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Apr 24 09:56:49 2020
-
-PBAS Scenario 2
+Created on Wed Apr 29 15:17:17 2020
+Scenario 2
+Each market produces at least 10% of the Capacity 
 """
 
-def NPV_SAA_Scenario2(Data,w,h):
-    import gurobipy as gb
-    from gurobipy import quicksum 
-    import numpy as np
-    import pandas as pd
-    
+import gurobipy as gb
+from gurobipy import quicksum
+import numpy as np
+
+def NPV_SAA2(Data, w, h):
     
     Scenarios = len(Data)
     Products = len(Data[0]['ProductSize'])
-    Time = len(Data[0]['ProductPrice'].columns)
-    
-    W = np.zeros((Products,Scenarios))
-    H = np.zeros((Products,Scenarios))
-    ## CONSTRAINTS
-    for p in range(Products):
-        for s in range(Scenarios):
-            W[p,s] = np.floor(w/(Data[s]['ProductSize'].values[p,3]+Data[s]['ProductSize'].values[p,4]))
-            H[p,s] = np.floor(h/(Data[s]['ProductSize'].values[p,2]+Data[s]['ProductSize'].values[p,4]))
-    
-    
+
+    Time = len(Data[0]['ProductPrice'].columns) - 2
+    Notebook = Data[0]['ProductSize'].loc[Data[0]['ProductSize']['Market']=='Notebook'].index.values
+    Monitor = Data[0]['ProductSize'].loc[Data[0]['ProductSize']['Market']=='Monitor'].index.values
+    Television = Data[0]['ProductSize'].loc[Data[0]['ProductSize']['Market']=='Television'].index.values
+    # Products Per Substrate
+    PoS = {}
+    for s in range(Scenarios):
+        PoS[s] = {}
+
+        for p in range(Products):
+            # Note: In our data Height<Width for all products, i.e. all products are oriented to be
+            # horizontal.
+
+            # Compute how many products fit if the screen is placed horizontally
+            num_width_hor = np.floor(w/Data[s]['ProductSize'].loc[p, 'Width (m)'])
+            num_height_hor = np.floor(h/Data[s]['ProductSize'].loc[p, 'Height (m)'])
+            num_products_hor = num_width_hor*num_height_hor
+
+            # Compute how many products fit if the screen is placed vertically
+            num_width_vert = np.floor(w/Data[s]['ProductSize'].loc[p, 'Height (m)'])
+            num_height_vert = np.floor(h/Data[s]['ProductSize'].loc[p, 'Width (m)'])
+            num_products_vert = num_width_vert*num_height_vert
+
+            if num_products_hor >= num_products_vert:
+                # Products are placed horizontally
+                PoS[s][p] = {'num_width': num_width_hor,
+                             'num_height': num_height_hor,
+                             'num_products': num_products_hor,
+                             'product_orientation': 'hor'}
+            else:
+                # Products are placed vertically
+                PoS[s][p] = {'num_width': num_width_vert,
+                             'num_height': num_height_vert,
+                             'num_products': num_products_vert,
+                             'product_orientation': 'vert'}
+
+    # INITIALIZE MODEL
     m = gb.Model('PBAS')
-    
-    ## VARIABLES
-    x = m.addVars(Products,Time,vtype=gb.GRB.INTEGER, lb= 0, name='Substrate per product over time')
-    ApS = m.addVars(Products,Scenarios,vtype = gb.GRB.INTEGER, lb=0,name = 'Products on Substrate')
-    GM = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS,name='GrossMargin')
-    OM = m.addVars(Scenarios, Time,vtype=gb.GRB.CONTINUOUS, name = 'Operating Margin')
-    Sales = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS, name = 'Sales')
-    CostofSales = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS, name = 'Cost of Sales')
-    NI = m.addVars(Scenarios, Time,vtype=gb.GRB.CONTINUOUS, name = 'Net Income')
-    NCF = m.addVars(Scenarios, Time,vtype=gb.GRB.CONTINUOUS, name = 'Net Cash Flow')
-    NPV = m.addVars(Scenarios,Time,vtype=gb.GRB.CONTINUOUS, name = 'Net Present Value')
-    CCC = m.addVars(Scenarios,vtype=gb.GRB.CONTINUOUS, name = 'Cash Conversion Cycle')
-    WC = m.addVars(Scenarios,Time,vtype=gb.GRB.CONTINUOUS, name = 'Working Capital')
-    DWC = m.addVars(Scenarios,Time,vtype=gb.GRB.CONTINUOUS, name = 'Delta Working Capital')
-    NPVperScenario = m.addVars(Scenarios,vtype=gb.GRB.CONTINUOUS, name = 'NPV per Scenario')
-    
-    ApS = {s: {p: W[p,s]*H[p,s] for p in range(Products)} for s in range(Scenarios)}        
-    
+
+    # VARIABLES
+    x = m.addVars(Products, Time, vtype=gb.GRB.INTEGER, lb=0,
+                  name='Substrate per product over time')
+    GM = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS, name='GrossMargin')
+    OM = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS, name='Operating Margin')
+    Sales = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS, name='Sales')
+    CostofSales = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS, name='Cost of Sales')
+    NI = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS, name='Net Income')
+    NCF = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS, name='Net Cash Flow')
+    NPV = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS, name='Net Present Value')
+    CCC = m.addVars(Scenarios, vtype=gb.GRB.CONTINUOUS, name='Cash Conversion Cycle')
+    WC = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS, name='Working Capital')
+    DWC = m.addVars(Scenarios, Time, vtype=gb.GRB.CONTINUOUS, name='Delta Working Capital')
+    NPVperScenario = m.addVars(Scenarios, vtype=gb.GRB.CONTINUOUS, name='NPV per Scenario')
+
+    # CONSTRAINTS
     for t in range(Time):
-        m.addConstr(quicksum(x[p,t] for p in range(Products)) <= Data[0]['MaxCapacity']*12,name='Yearly Substrate Capacity')
-    
-    for t in range(2,Time):
-        m.addConstr(quicksum(x[p,t] for p in range(2)) >= 0.1*Data[0]['MaxCapacity']*12 ) #NOTEBOOKS
-        m.addConstr(quicksum(x[p,t] for p in range(2,5)) >= 0.1*Data[0]['MaxCapacity']*12 ) #MONITOR
-        m.addConstr(quicksum(x[p,t] for p in range(5,Products)) >= 0.1*Data[0]['MaxCapacity']*12 ) #TV
-            
-    for s in range(Scenarios):
-        for t in range(Time):
-            m.addConstr(Sales[s,t] == quicksum(Data[s]['ProductPrice'].values[p,t]*Data[s]['Yield'].values[p,t]*ApS[s][p]*x[p,t] for p in range(Products)),name='Sales constraint')
-                
-    for s in range(Scenarios):
-        for t in range(Time):         
-            m.addConstr(CostofSales[s,t] == quicksum(Data[s]['SubstrateCost'].values[0,t]*(w*h)*x[p,t]    for p in range(Products)) + Data[s]['Depreciation'].values[0,t],name='Cost of Sales constraint')
-    
-    for s in range(Scenarios):
-        for t in range(Time):
-            GM[s,t] = Sales[s,t]-CostofSales[s,t]
+        m.addConstr(quicksum(x[p, t] for p in range(Products)) <= Data[0]
+                    ['MaxCapacity']*12, name='Yearly Substrate Capacity')
         
+    for t in range(2,Time): # Start at t = 2, yield for t = 0 and 1 is equal to 0
+        m.addConstr(quicksum(x[p,t] for p in Notebook) >= 0.1*Data[0]['MaxCapacity']*12 ) #NOTEBOOKS
+        m.addConstr(quicksum(x[p,t] for p in Monitor) >= 0.1*Data[0]['MaxCapacity']*12 ) #MONITOR
+        m.addConstr(quicksum(x[p,t] for p in Television) >= 0.1*Data[0]['MaxCapacity']*12 ) #TV
+
+
     for s in range(Scenarios):
         for t in range(Time):
-            OM[s,t] = GM[s,t] - Sales[s,t]*(Data[s]['R&D']+Data[s]['SG&A'])
-    
-    for s in range(Scenarios):
-        for t in range(Time):
-            NI[s,t] = (1-Data[s]['TaxRate'])*OM[s,t]
-    
-    for s in range(Scenarios):
+            m.addConstr(Sales[s, t] == quicksum(
+                Data[s]['ProductPrice'].iloc[p, t+2] *
+                Data[s]['Yield'].iloc[p, t+4]*PoS[s][p]['num_products']*x[p, t]
+                for p in range(Products)), name='Sales constraint')
+
+            m.addConstr(CostofSales[s, t] == quicksum(
+                Data[s]['SubstrateCost'].iloc[0, t]*(w*h)*x[p, t] for p in range(Products)) +
+                Data[s]['Depreciation'].iloc[0, t], name='Cost of Sales constraint')
+
+            GM[s, t] = Sales[s, t]-CostofSales[s, t]
+            OM[s, t] = GM[s, t] - Sales[s, t]*(Data[s]['R&D']+Data[s]['SG&A'])
+            NI[s, t] = (1-Data[s]['TaxRate'])*OM[s, t]
+            WC[s, t] = Sales[s, t]*CCC[s]
+            NCF[s, t] = (NI[s, t] + Data[s]['Depreciation'].iloc[0, t] - DWC[s, t] -
+                         Data[s]['InvestmentCost'].iloc[0, t])
+            NPV[s, t] = NCF[s, t]/((1+Data[s]['WACC'])**t) # TODO: Check whether this should be **(t+1)
+
+        # These constraints are only made for every s
         CCC[s] = (Data[s]['DIO']+Data[s]['DSO']-Data[s]['DPO'])/365
-    
-    for s in range(Scenarios):
-        for t in range(Time):
-            WC[s,t] = Sales[s,t]*CCC[s]
-    
-    for s in range(Scenarios):
-        for t in range(1,Time):
-            DWC[s,0] = WC[s,0]
-            DWC[s,t] = WC[s,t-1]-WC[s,t]
-                    
-    for s in range(Scenarios): ## DWC moet er nog bij.
-        for t in range(Time):
-            NCF[s,t] = NI[s,t] + Data[s]['Depreciation'].values[0,t] - DWC[s,t] - Data[s]['InvestmentCost'].values[0,t]
-    
-    for s in range(Scenarios): 
-        for t in range(Time):
-            NPV[s,t] = NCF[s,t]/((1+Data[s]['WACC'])**t)
-    
-    for s in range(Scenarios):
-        NPVperScenario[s] = quicksum(NPV[s,t] for t in range(Time))
-    
-    
-    
-    ## OBJECTIVE
-    
-    obj = gb.LinExpr()
-    obj = (1/Scenarios)*quicksum(quicksum(NPV[s,t] for s in range(Scenarios))for t in range(Time))
-    
+        NPVperScenario[s] = quicksum(NPV[s, t] for t in range(Time))
+        DWC[s, 0] = WC[s, 0]
+        
+        for t in range(1, Time):
+            DWC[s, t] = WC[s, t-1]-WC[s, t]
+
+    # OBJECTIVE
+    obj = (1/Scenarios)*quicksum(quicksum(NPV[s, t] for s in range(Scenarios)) for t in range(Time))
     m.setObjective(obj, gb.GRB.MAXIMIZE)
-    
+
+    # RUN OPTIMIZATION
     m.optimize()
-    
-    
+
+    # Count negative scenarios
     NegativeScenario = 0
     for s in range(Scenarios):
-        if NPVperScenario[s].getValue() <= 0:
-           NegativeScenario = NegativeScenario + 1
-    
-    return {'Average NPV' :obj.getValue(),
-            'Width' :w,
-            'Height':h,
-            '#NegativeScenarios':NegativeScenario}
+        if NPVperScenario[s].getValue() < 0:
+            NegativeScenario = NegativeScenario + 1
 
+    return {'Average NPV': obj.getValue(),
+            'Width': w,
+            'Height': h,
+            '#NegativeScenarios': NegativeScenario}
